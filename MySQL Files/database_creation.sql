5c6a845eea6a117a3330c8a47c1f4206603bf901
Carlos Del Carpio <cdeclarp@uncc.edu>
2021-06-22 21:03:05 -0400

    mySQL creation file has been updated to contain the creation of user and product table. Also creates dummy data.

--- MySQL Files/database_creation.sql
+++ MySQL Files/database_creation.sql
@@ -7,14 +7,31 @@ CREATE TABLE user (
 	UserID INT NOT NULL AUTO_INCREMENT,
     FirstName VARCHAR(50), 
     LastName VARCHAR(50),
-    email VARCHAR(100),
-    password VARCHAR(50),
+    UserName VARCHAR(50),
+    Password VARCHAR(50),
+    Admin INT NOT NULL,
     PRIMARY KEY(UserID)
 );
 
 INSERT INTO user 
-	(FirstName, LastName, email, password)
+	(FirstName, LastName, UserName, Password, Admin)
 VALUES
-	('Alice', 'Smith', 'a@a.com', 'test123'),
-    ('Bob', 'Smith', 'b@b.com', 'test123'),
-    ('Charlie', 'Smith', 'c@c.com', 'test123');
+	('Alice', 'Smith', 'asmith', 'test123', 0),
+    ('Bob', 'Smith', 'bsmith', 'test123', 0),
+    ('Charlie', 'Smith', 'csmith', 'test123', 0),
+    ('admin', 'admin', 'admin', 'admin', 1);
+  
+  
+CREATE TABLE product (
+	ProductID INT NOT NULL AUTO_INCREMENT,
+    Name VARCHAR(50),
+    Price DOUBLE,
+    ImagePath VARCHAR(200),
+    PRIMARY KEY(ProductID)
+);
+
+INSERT INTO product
+	(Name, Price, ImagePath)
+VALUES
+	('PlayStation 5', 499.99, 'c:/images/ps5.jpeg'),
+	('M1 Mac', 789.99, 'c:/images/m1Mac.jpeg');
